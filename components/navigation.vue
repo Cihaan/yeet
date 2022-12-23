937ddf44c83debb69c17e8de2bb2e01096702ff7
Cihaan <cihankafadar1@gmail.com>
2022-12-23 22:53:53 +0100

    chore: setup folders

---
+++ components/navigation.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+       <ul>
+        <li>Recipes list</li>
+        <li>Github</li>
+       </ul> 
+    </div>
+</template>
+
+<script setup lang="ts">
+const recipes = ref([])
+
+
+</script>
+<style lang="scss" scoped>
+
+</style>
