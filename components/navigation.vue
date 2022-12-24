dfccfd7b6ee9ac4b1678431b29278c45af3a9eed
Cihaan <cihankafadar1@gmail.com>
2022-12-24 10:35:53 +0100

    feat: navigation

--- components/navigation.vue
+++ components/navigation.vue
@@ -1,17 +1,58 @@
 <template>
-    <div>
-       <ul>
-        <li>Recipes list</li>
-        <li>Github</li>
-       </ul> 
-    </div>
+  <div>
+    <ul>
+        <p @click="onclick">Yeet</p>
+      <nav>
+        <nuxt-link  to="/recipes">Recipes</nuxt-link>
+        <a href="https://github.com/Cihaan/yeet">Github</a>
+      </nav>
+    </ul>
+  </div>
 </template>
 
 <script setup lang="ts">
-const recipes = ref([])
 
+const onclick = () => {
+    const router = useRouter()
+    router.push('/recipes')
+}
 
 </script>
 <style lang="scss" scoped>
+ul {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+  margin: 0;
+  p {
+    font-size: 2rem;
+    font-weight: 600;
+    color: var(--green);
+    cursor: pointer;
 
+    &:hover {
+      color: var(--green-hover);
+    }
+  }
+  nav {
+    display: flex;
+    gap: 10px;
+  }
+  a {
+    text-decoration: none;
+    padding: 1rem;
+    font-size: 1.5rem;
+    font-weight: 600;
+    color: var(--gray);
+    cursor: pointer;
+    &:hover {
+      color: var(--green-hover);
+    }
+
+    &.nuxt-link-active {
+      color: var(--green);
+    }
+  }
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  // add scss config for nuxt here
+  css: ["~/assets/main.scss"],
+});
